40e8ef89fdffcf42671eedbad6685fcb753898be
Ting <reruin@gmail.com>
2021-01-13 17:26:50 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,8 @@
-killall node
-killall nodejs
-
-ls
-
-cat /sharelist/cache/config.json
-echo "$CONFIG"
-
 if echo "$CONFIG" | grep -q -i "^http"; then
   wget --no-check-certificate $CONFIG -O /sharelist/cache/config.json
 else
   echo -e "$CONFIG" > /sharelist/cache/config.json
 fi
-cat /sharelist/cache/config.json
+
 cd /sharelist
 npm start
